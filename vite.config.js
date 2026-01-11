3f415f7d9965b622baf377a3112df25d9bee6de6
gracelsy <43689473+gracelsy@users.noreply.github.com>
2026-01-11 11:29:22 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: 'site',
-  publicDir: 'public',
+  base: "/",
   build: {
-    outDir: '../dist',
-    emptyOutDir: true
+    outDir: "dist"
   }
-})
+});
